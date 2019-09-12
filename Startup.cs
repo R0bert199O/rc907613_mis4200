1d2cd5215b27aa397023d1a036005ff13d57064c
Robert Crow - Home <rc907613@ohio.edu>
2019-09-12 19:24:06 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(rc907613_mis4200.Startup))]
+namespace rc907613_mis4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
