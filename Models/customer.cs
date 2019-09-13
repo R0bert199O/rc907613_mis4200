8bf33c79355ea151199ec728305ae6dceaf5289a
rc907613 <rc907613@ohio.edu>
2019-09-13 10:57:05 -0400

    recreated customers and order models in order to catch back up

---
+++ Models/customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crow_MIS4200.Models
+{
+    public class customer
+    {
+        public int customerId { get; set; }
+        //primary key by default, will pick ID first, and then after that it will look for a classId (customerId)
+        public int firstName { get; set; }
+        public int lastName { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+
+        //customer can have many orders
+
+        public ICollection<order> orders { get; set; }
+
+        //ICollection I: Interface
+        //if you see <> it wants a data type. In this case order from our order class
+        //this will show all orders placed by this customer
+    }
+}
---
+++ Models/order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crow_MIS4200.Models
+{
+    public class order
+    {
+        public int orderId { get; set; }
+        //primary key by default, will pick ID first, and then after that it will look for a classId (customerId)
+        public int customerId { get; set; }
+        public virtual customer customer { get; set; }
+        //virtual causes it to load as needed, if you just want it to load from the beginning, take out virtual
+        //datatype can be any object. Embedding the foreign object "customer" information in the order table
+        public DateTime orderDate { get; set; }
+     }
+}
