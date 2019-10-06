16a09a67c3e170002585959b22bf419309d36088
Robert Crow <rc907613@ohio.edu>
2019-10-06 18:30:31 -0400

    Fixed Data Validation & Republished

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -22,7 +22,7 @@ namespace rc907613_mis4200.Models
         public string eventLocation { get; set; }
 
         [Display(Name = "Date")]
-        [RegularExpression(@"^(((0?[1-9]|1[012])/(0?[1-9]|1\d|2[0-8])|(0?[13456789]|1[012])/(29|30)|(0?[13578]|1[02])/31)/(19|[2-9]\d)\d{2}|0?2/29/((19|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(([2468][048]|[3579][26])00)))$", ErrorMessage = "Must be in MM/dd/yyyy format")]
+        [DataType(DataType.Date, ErrorMessage = "Date must be in mm/dd/yyyy format")]
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime eventDate { get; set; }
 
